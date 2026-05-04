416ced025ea16410037c2e02527b0e3ab4129d42
stnvtrani <sitinovitriani@gmail.com>
2026-05-04 16:01:18 +0700

    Tugas Modul 3

---
+++ modul 3/Biodata.php
@@ -0,0 +1,10 @@
+<?php
+$nim = '202457201065';
+$nama = 'Triani';
+$alamat = 'Bandar Alim';
+$nilai = 100;
+echo "NIM :", $nim, "</br>";
+echo "Nama :", $nama, "</br>";
+echo "Alamat :", $alamat, "</br>";
+echo "Nilai :", $nilai, "</br>";
+?>
---
+++ modul 3/ketiga.php
@@ -0,0 +1,29 @@
+<?php
+$a = "5";
+$b = "2.5";
+$komentar = "Selamat Datang ";
+//variabel bertipe integer
+echo ("Nilai variabel a adalah = $a <br>");
+//variabel bertipe real
+echo ("Nilai variabel b adalah = $b <br>");
+//varaibel bertipe string
+$tambah= $a + $b;
+//rumus penjumlahan
+$kurang = $a - $b;
+//rumus pengurangan
+$kali= $a + $b;
+//rumus perkalian
+$bagi= $a - $b;
+//rumus pembagian
+
+echo ("Hasil penjumlahan a dan b adalah = $tambah <br>");
+echo ("Hasil pengurangan a dan b adalah = $kurang <br>");
+echo ("Hasil perkalian a dan b adalah = $kali <br>");
+echo ("Hasil pembagian a dan b adalah = $bagi <br>");
+$nama = "Triani";
+$garis = "==========================";
+echo "<p>";
+echo $garis."<br>";
+echo $komentar. "Di lab ". $nama. "<br>Selamat Belajar Pemrograman Web <br>";
+echo $garis. "<br>";
+?>
