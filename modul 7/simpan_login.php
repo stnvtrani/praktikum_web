ac975ed73dd621b254a224af9d70d64d79cbf9e1
stnvtrani <sitinovitriani@gmail.com>
2026-05-24 18:39:38 +0700

    tugas2

---
+++ modul 7/simpan_login.php
@@ -0,0 +1,7 @@
+<?php
+echo "
+<body style='font-family: sans-serif; background-color: #fdfaf2; padding: 50px; display: flex; justify-content: center;'>
+        <b style='color: #2563eb; font-size: 18px;'>~ ~ SELAMAT ANDA BERHASIL LOGIN ~ ~</b>      
+</body>
+";
+?>
